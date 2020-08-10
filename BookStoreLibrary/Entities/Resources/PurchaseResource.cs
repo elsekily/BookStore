ba45a40843eaf83b769cf99a1a9aaf621ee340f6
Youssef Elsekily <youssefelsekily@outlook.com>
2020-08-10 18:01:34 +0200

    add Purchase Resources

---
+++ BookStoreLibrary/Entities/Resources/PurchaseResource.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace BookStoreLibrary.Entities.Resources
+{
+    public class PurchaseResource
+    {
+        public int ID { get; set; }
+        [Range(0, int.MaxValue)]
+        public double Price { get; set; }
+        [Range(0, int.MaxValue)]
+        public double Discount { get; set; }
+        [Range(0, int.MaxValue)]
+        public double TotalPrice { get; set; }
+        public DateTime TimeIssued { get; set; }
+        public string AddedBy { get; set; }
+        public IList<PurchaseBookResource> Books { get; set; }
+        public PurchaseResource()
+        {
+            Books = new List<PurchaseBookResource>();
+        }
+    }
+}
---
+++ BookStoreLibrary/Entities/Resources/PurchaseSaveResource.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace BookStoreLibrary.Entities.Resources
+{
+    public class PurchaseSaveResource
+    {
+        [Range(1, int.MaxValue)]
+        public double Price { get; set; }
+        [Range(1, int.MaxValue)]
+        public double Discount { get; set; }
+        [Range(0, int.MaxValue)]
+        public double TotalPrice { get; set; }
+        public IList<PurchaseBookResource> Books { get; set; }
+        public PurchaseSaveResource()
+        {
+            Books = new List<PurchaseBookResource>();
+        }
+    }
+}
